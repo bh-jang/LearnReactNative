6dfb221187b6fa96e7af39bdc72971ffb805ec51
hoony.jang <hoony.jang@goodoc.co.kr>
2023-09-21 16:04:10 +0900

    fix: TabNavigation Header Button

---
+++ src/screens/Main/WebViewScreen/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const WebViewScreen = () => {
+  return (
+    <View>
+      <Text>WebViewScreen</Text>
+    </View>
+  )
+}
+
+export default WebViewScreen
+
+const styles = StyleSheet.create({})
--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BottomTabHeaderProps, BottomTabNavigationOptions, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './TabScreens/Home';
+import WebViewScreen from './WebViewScreen';
+import { Button } from '@react-native-material/core';
+import { useNavigation } from '@react-navigation/native';
 
 interface MainTabParamList {
   HomeScreen: undefined;
-  FirstScreen: undefined;
+  WebViewScreen: undefined;
   SecondScreen: undefined;
   ThirdScreen: undefined;
 }
@@ -13,12 +16,18 @@ const Tab = createBottomTabNavigator<MainTabParamList>();
 
 const Main = () => {
 
+  const navigation = useNavigation();
+
+  const tabScreenOptions: BottomTabNavigationOptions = {
+    headerLeft: () => (<Button variant='outlined' title='뒤로가기' onPress={() => navigation.goBack()} />)
+  }
+
   return (
     <Tab.Navigator>
-      <Tab.Screen name='HomeScreen' component={Home} />
-      <Tab.Screen name='FirstScreen' component={Home} />
-      <Tab.Screen name='SecondScreen' component={Home} />
-      <Tab.Screen name='ThirdScreen' component={Home} />
+      <Tab.Screen name='HomeScreen' options={tabScreenOptions} component={Home} />
+      <Tab.Screen name='WebViewScreen' options={tabScreenOptions} component={WebViewScreen} />
+      <Tab.Screen name='SecondScreen' options={tabScreenOptions} component={Home} />
+      <Tab.Screen name='ThirdScreen' options={tabScreenOptions} component={Home} />
     </Tab.Navigator>
   )
 }
